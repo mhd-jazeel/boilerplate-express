0cb717cf39acdbe75be2ae6e5b64adffb1eea773
mhd-jazeel <mohammadjazeel3@gmail.com>
2025-07-17 06:09:14 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 
 
